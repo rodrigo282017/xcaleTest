93b2fa691051f4b80c764c4f91cf8fb4f8a2e525
RodrigoTorres <rodrigotorresrittatore@gmail.com>
2023-09-26 23:13:30 +0200

    Added circleCi integration folder

--- src/test/java/com/xcale/xcaletest/XcaleTestApplicationTests.java
+++ src/test/java/com/xcale/xcaletest/XcaleTestApplicationTests.java
@@ -1,9 +1,10 @@
 package com.xcale.xcaletest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootApplication
 class XcaleTestApplicationTests {
 
 	@Test
